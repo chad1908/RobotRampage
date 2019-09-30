c44eab72d7b97df0840726d05bdf5b29e105e147
Chad McGillivray [Student] <chad.mcgillivray@student.sl.on.ca>
2019-09-30 15:40:24 -0400

    Added gun scripts to make them selectable

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants
+{
+    //scnenes
+    public const string SceneBattle = "Battle";
+    public const string SceneMenu = "MainMenu";
+
+    // Gun Types
+    public const string Pistol = "Pistol";
+    public const string Shotgun = "Shotgun";
+    public const string AssaultRifle = "AssaultRifle";
+
+    //Robot Types
+    public const string Redrobot = "Redrobot";
+    public const string Bluerobot = "Bluerobot";
+    public const string Yellowrobot = "Yellowrobot";
+
+    // Pickup Types
+    public const int PickupPistolAmmo = 1;
+    public const int PickupAssaultAmmo = 2;
+    public const int PickupShotgunAmmo = 3;
+    public const int PickupHealth = 4;
+    public const int PickupArmour = 5;
+
+    //Misc
+    public const string Game = "Game";
+    public const float CameraDefaultZoom = 60f;
+
+    public static readonly int[] AllPickupTypes = new int[5]
+    {
+       PickupPistolAmmo,
+       PickupAssaultAmmo,
+       PickupShotgunAmmo,
+       PickupHealth,
+       PickupArmour
+    };
+}
+
