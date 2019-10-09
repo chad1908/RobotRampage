2723c695c2ec4829f954d5c147f42309308a8a67
chad1907 <hockey_1908@hotmail.com>
2019-10-08 20:34:07 -0400

    finished creating the robots (section 1 chapter 10 end P. 272)

---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Robot : MonoBehaviour
+{
+    [SerializeField]
+    private string robotType;
+
+    public int health;
+    public int range;
+    public float fireRate;
+    public Transform missileFireSpot;
+    UnityEngine.AI.NavMeshAgent agent;
+    private Transform player;
+    private float timeLastFired;
+    private bool isDead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //sets the agent and player values to the navmesh agent and player componenets.
+        isDead = false;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //checks if the robot is dead before starting.
+        if (isDead)
+        {
+            return;
+        }
+
+        //makes the robot face the player
+        transform.LookAt(player);
+        //tells the robot to use the navmesh to find the player.
+        agent.SetDestination(player.position);
+        //check to see if the robot is within firing range and if there’s been enough time between shots to fire again.
+        if (Vector3.Distance(transform.position, player.position) < range
+        && Time.time - timeLastFired > fireRate)
+        {
+            //update timeLastFired to the current time and call Fire(), which simply logs a message to the console for the time being.
+            timeLastFired = Time.time;
+            fire();
+        }
+    }
+
+    private void fire()
+    {
+        Debug.Log("Fire");
+    }
+}
