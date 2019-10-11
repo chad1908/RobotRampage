0b3fcf55210732473f4e547f04de98ddbfc01d65
chad1907 <hockey_1908@hotmail.com>
2019-10-10 22:22:44 -0400

    Small commit Visual studio was not taking my code, will finish in class

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int health;
+    public int armor;
+    public GameUI gameUI;
+    private GunEquipper gunEquipper;
+    private Ammo ammo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ammo = GetComponent<Ammo>();
+        gunEquipper = GetComponent<GunEquipper>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    
+}
