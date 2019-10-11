3576b0714a9efbcf8bcdab503b5ff006aeedea86
Chad McGillivray [Student] <chad.mcgillivray@student.sl.on.ca>
2019-10-11 13:56:01 -0400

    Finished Robot spawning (end P.294)

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private static Game singleton;
+
+    [SerializeField]
+    RobotSpawn[] spawns;
+
+    public int enemiesLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        singleton = this;
+        SpawnRobots();
+    }
+
+    private void SpawnRobots()
+    {
+        foreach (RobotSpawn spawn in spawns)
+        {
+            spawn.SpawnRobot();
+            enemiesLeft++;
+        }
+    }
+}
