4d2e4cea45d33baed4cd37b6154532d61afc4196
Chad McGillivray [Student] <chad.mcgillivray@student.sl.on.ca>
2019-10-11 13:11:45 -0400

    Finished Adding damage effects (end P. 282)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -23,5 +23,27 @@ public class Player : MonoBehaviour
 
     }
 
-    
+    public void TakeDamage(int amount)
+    {
+        int healthDamage = amount;
+        if (armor > 0)
+        {
+        int effectiveArmor = armor * 2;
+        effectiveArmor -= healthDamage;
+        // If there is still armor, don't need to process
+        // health damage
+        if (effectiveArmor > 0)
+        {
+        armor = effectiveArmor / 2;
+        return;
+        }
+            armor = 0;
+        }
+        health -= healthDamage;
+        Debug.Log("Health is " + health);
+        if (health <= 0)
+        {
+            Debug.Log("GameOver");
+        }
+    }
 }
